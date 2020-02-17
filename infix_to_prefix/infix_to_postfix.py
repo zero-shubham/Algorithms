9a359020df3390c83cdcae8c4a80099325522697
Shubham Biswas <shubhambiswas.zero@gmail.com>
2020-02-17 12:35:24 +0530

    fixed fix_multi_digits logic

--- infix_to_prefix/infix_to_postfix.py
+++ infix_to_prefix/infix_to_postfix.py
@@ -1,19 +1,15 @@
+import re
+
+
 def is_operator(val):
     if val == "+" or val == "-" or val == "/" or val == "*" or val == "(" or val == ")":
         return True
     return False
 
 
-def fix_double_digits(infix_expr):
-    for idx, _ in enumerate(infix_expr):
-        if idx+1 < len(infix_expr) and not is_operator(
-            infix_expr[idx]
-        ) and not is_operator(
-            infix_expr[idx+1]
-        ):
-            tmp = infix_expr[idx] + infix_expr[idx+1]
-            infix_expr.pop(idx+1)
-            infix_expr[idx] = tmp
+def fix_multi_digits(infix_expr):
+    infix_expr = re.split(r"(\*|\+|\-|/|\(|\)){1}", infix_expr)
+    infix_expr = list(filter(None, infix_expr))
     return infix_expr
 
 
@@ -49,10 +45,9 @@ def add_to_stack(val, stack, postfix):
 
 
 def to_postfix(infix_expr):
-    infix_expr = list(infix_expr)
+    infix_expr = fix_multi_digits(infix_expr)
     stack = []
     postfix = []
-    infix_expr = fix_double_digits(infix_expr)
 
     for val in infix_expr:
         if not is_operator(val):
