74db23ebafe461faabc3bcfa589e005205b285c1
Shubham Biswas <shubhambiswas_zero@yahoo.com>
2025-04-07 20:17:12 +0530

    house robber fix

--- house_robber/main.py
+++ house_robber/main.py
@@ -2,36 +2,14 @@ from typing import List
 
 
 class Solution:
-    def get_largest_index(self, arr: List[int]) -> int:
-        largest = 0
-        for i in range(1, len(arr)):
-            if arr[i] > arr[largest]:
-                largest = i
-
-        return largest
 
     def rob(self, nums: List[int]) -> int:
-        print(nums)
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-
-        largest_idx = self.get_largest_index(nums)
-        first_half = []
-        if largest_idx-1 > 0:
-            first_half = nums[:largest_idx-1]
-
-        second_half = []
-        if largest_idx+1 < len(nums):
-            second_half = nums[largest_idx+2:]
-        print("largest idx: ", largest_idx)
-        print("first half: ", first_half)
-        print("second half: ", second_half)
-        largest_num = self.rob(
-            first_half) + nums[largest_idx] + self.rob(
-            second_half)
-        return largest_num
+        l1, l2 = 0, 0
+        for i in nums:
+            tmp = max(l1+i, l2)
+            l1 = l2
+            l2 = tmp
+        return max(l1, l2)
 
 
 def main():
